c6ed2ada1978c35ac443e8af3cd05075653987b9
Rishabh Gupta <rishabh.gupta.1606@gmail.com>
2018-04-05 17:44:51 -0400

    relation map setup

--- P1/Statistics.h
+++ P1/Statistics.h
@@ -1,26 +1,32 @@
 #ifndef STATISTICS_
 #define STATISTICS_
 #include "ParseTree.h"
-
+#include <unordered_map>
+#include <string>
+struct RelationInfo
+{
+	int numTuples;
+	std::unordered_map<std::string, int> attributeMap;
+} typedef RelationInfo;
 
 class Statistics
 {
-public:
+  private:
+	std::unordered_map<std::string, RelationInfo> relationMap;
+
+  public:
 	Statistics();
-	Statistics(Statistics &copyMe);	 // Performs deep copy
+	Statistics(Statistics &copyMe); // Performs deep copy
 	~Statistics();
-
-
 	void AddRel(char *relName, int numTuples);
 	void AddAtt(char *relName, char *attName, int numDistincts);
 	void CopyRel(char *oldName, char *newName);
-	
+
 	void Read(char *fromWhere);
 	void Write(char *fromWhere);
 
-	void  Apply(struct AndList *parseTree, char *relNames[], int numToJoin);
+	void Apply(struct AndList *parseTree, char *relNames[], int numToJoin);
 	double Estimate(struct AndList *parseTree, char **relNames, int numToJoin);
-
 };
 
 #endif
