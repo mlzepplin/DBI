52f2452d1baad91260abb0a1ee24cbc146560f76
krishna <krishnachaitanya265@gmail.com>
2018-04-19 15:40:31 -0400

    add intial code for a4-2

--- P1/main.cc
+++ P1/main.cc
@@ -1,19 +1,16 @@
+
 #include <iostream>
-#include "Record.h"
-#include <stdlib.h>
-#include "HeapFile.h"
+#include "ParseTree.h"
+
 using namespace std;
 
 extern "C" {
 	int yyparse(void);   // defined in y.tab.c
 }
 
-extern struct AndList *final;
-
 int main () {
-	HeapFile *h = new HeapFile();
-    	Schema mySchema ("catalog", "lineitem");
-	h->Load(mySchema,"./lineitem.tbl");
 
+	yyparse();
 }
+
 
